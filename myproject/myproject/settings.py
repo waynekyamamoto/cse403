e4a7ebb3e9ca3193b203a6dfa630c0ea4b873332
Wayne Yamamoto <yamamoto@charityblossom.org>
2012-07-26 15:12:43 -0700

    Addeded all the basic setup stuff

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'yamamoto_db',                      # Or path to database file if using sqlite3.
+        'USER': 'yamamoto',                      # Not used with sqlite3.
+        'PASSWORD': 'yamamoto',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'myapp'
 )
 
 # A sample logging configuration. The only tangible logging
